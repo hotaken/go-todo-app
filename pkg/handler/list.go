7d69b8cf99a40ab91a5d19b9895389d7847292dc
hotaken <soland777@ya.ru>
2023-03-01 03:40:36 +0500

    jwt parsing

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -1,6 +1,8 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) createList(c *gin.Context) {
 
@@ -21,4 +23,3 @@ func (h *Handler) updateList(c *gin.Context) {
 func (h *Handler) deleteList(c *gin.Context) {
 
 }
-
